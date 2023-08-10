63ae72e5a8d89c9f571bb5bb9470acf75c424e17
Absoly <nomer3182@gmail.com>
2023-08-10 20:06:19 +0500

    Новые ящики

--- core/crate.gd
+++ core/crate.gd
@@ -3,7 +3,7 @@ extends RigidBody2D
 
 @export var weapon_scene : Array[PackedScene]
 
-func _on_area_2d_body_entered(body):
+func _on_body_entered(body):
 	if body.has_method("pick_weapon"):
 		var weapon = weapon_scene.pick_random().instantiate()
 		body.call_deferred("pick_weapon", weapon)
